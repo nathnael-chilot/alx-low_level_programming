f93fea711b8d3bc7250a5e105630deb2439c5493
nathnael-chilot <chilotnathnael@gmail.com>
2022-10-14 06:46:36 -0700

    create 0x00-hello_world 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (success)
+ *
+ *     */
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
