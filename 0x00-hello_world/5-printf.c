7525fbe24bd45fbbe32ff72568b3708b7a7b5dc1
nathnael-chilot <chilotnathnael@gmail.com>
2022-10-14 07:30:40 -0700

    create 0x00-hello_world5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - print out string
  *
- * return 0 if success
+ * Return: 0 if success
  *
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n);
 	return (0);
 }
