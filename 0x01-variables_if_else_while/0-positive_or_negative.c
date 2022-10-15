b6254b7412b45039b6649be1415153ae4f14eeb6
nathnael-chilot <chilotnathnael@gmail.com>
2022-10-15 05:31:31 -0700

    create c-variables,if,else,while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,12 +21,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
-
 	return (0);
 }
