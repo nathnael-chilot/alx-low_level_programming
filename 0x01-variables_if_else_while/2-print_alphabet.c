9695aab87a8c1c7232016c1f24b0c70609bc030e
nathnael-chilot <chilotnathnael@gmail.com>
2022-10-15 06:21:01 -0700

    create c-variables,if,else,while 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if success
+ *
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while(ch <= 'z')
+	{
+		putchar(ch);
+	}
+
+	putchar("\n");
+
+	return (0);
+
+}
