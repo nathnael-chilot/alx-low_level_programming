f02166cb6a59d395ecbb2a39364e9ecd4394f528
nathnael-chilot <chilotnathnael@gmail.com>
2022-10-17 07:59:30 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Return: 0 if success
+ *
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 98; i++)
+	{
+
+		for (j = i + 1; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 98 && j == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
