b28c2c2826dbf481b27941b75eec1d24e3c54a7d
nathnael-chilot <chilotnathnael@gmail.com>
2022-10-17 07:37:31 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,9 +16,9 @@ int main(void)
 	for (i = 0; i < 8; i++)
 	{
 
-		for (j = i+1; j < 9; j++)
+		for (j = i + 1; j < 9; j++)
 		{
-			for (k = j+1; k < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
