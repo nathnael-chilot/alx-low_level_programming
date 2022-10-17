1ed5de2a615156afb29a9611f81dece8bef7826c
nathnael-chilot <chilotnathnael@gmail.com>
2022-10-17 10:21:52 -0700

    0x00-hello_world 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@
 
 int main(void)
 {
-
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
