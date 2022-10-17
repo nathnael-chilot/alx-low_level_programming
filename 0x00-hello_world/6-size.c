99a39eb378ab2afd393d6da31e389d1dcea245b6
nathnael-chilot <chilotnathnael@gmail.com>
2022-10-17 13:05:53 -0700

    0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard error.
+ * main - entry point
  *
  * Return: 0 if success
  *
@@ -13,7 +12,9 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));        printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a long long int: %zu byte(s)\n",
+		       	sizeof(long long int));       
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
