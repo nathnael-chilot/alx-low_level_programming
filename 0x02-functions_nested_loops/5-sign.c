ffba533ad3326f9d0ca23df52dedc7d2d7a259e3
nathnael-chilot <chilotnathnael@gmail.com>
2022-10-18 11:00:09 -0700

    create function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,9 @@
 
 /**
  * print_sign - prints the sign of a number.
- *
- * Returns 1 and prints + if n is greater than zero
+ * 
+ * @n: the number to be checked
+ * Return: 1 and prints + if n is greater than zero
  *
  * Returns 0 and prints 0 if n is zero
  *
