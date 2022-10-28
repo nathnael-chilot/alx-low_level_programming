264aa7d32be5c4caffdc10e957a85a37a9970474
nathnael-chilot <chilotnathnael@gmail.com>
2022-10-28 12:43:42 -0700

    Write a function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, diff = 0;
 
-	while(1)
+	while (1)
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
@@ -33,7 +33,7 @@ int _strcmp(char *s1, char *s2)
 			diff = s1[i] - s2[i];
 			break;
 		}
-		else 
+		else
 			i++;
 	}
 	return (diff);
