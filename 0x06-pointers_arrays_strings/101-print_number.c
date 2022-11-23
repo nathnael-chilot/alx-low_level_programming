0d529f74c02f8b979673c5049dfb78c19eb0c5d2
nathnael-chilot <chilotnathnael@gmail.com>
2022-11-22 22:25:36 -0800

    create 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,41 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer;
- * @n: integer to be printed;
- */
+* print_number - prints an integer
+* @n: int to be printed
+* Return: nothing
+**/
+
 void print_number(int n)
 {
-	unsigned int n1;
+	int dup, neg;
 
-	if (n < 0)
+	dup = 1000000000;
+	neg = 1;
+
+	if (n > 0)
 	{
-		n1 = -n;
-		_putchar('-');
+		neg *= -1;
+		n *= -1;
+	}
+
+	if (n != 0)
+	{
+		while (n / dup == 0)
+		{
+			dup = dup / 10;
+		}
+		if (neg == 1)
+		{
+			_putchar('-');
+		}
+		while (dup >= 1)
+		{
+			_putchar(-(n / dup) + '0');
+			n = n % dup;
+			dup = dup / 10;
+		}
 	}
 	else
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-		print_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');
+		_putchar('0');
 }
